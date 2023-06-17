61eb1c946cd0edfc24a2b63c4e539e785ff24d8c
FOWMind <alwl1@hotmail.com>
2023-06-17 09:25:14 -0300

    [ADD] animations

--- js/index.js
+++ js/index.js
@@ -1,6 +1,80 @@
+const documentTitle = document.title
+const modalContainer = document.getElementById('modal-container')
+const view = document.getElementById('view')
+const elements = document.getElementsByClassName('elements')[0]
+
+// Save each pick element and score
+const paper = document.getElementById('paper')
+const scissors = document.getElementById('scissors')
+const rock = document.getElementById('rock')
+const score = document.getElementById('score')
+const openModal = document.getElementById('open-modal')
+
+// Detect score changes for saving it in localStorage
+const observer = new MutationObserver((mutations) => {
+	const innerText = mutations[0].target.innerText
+	if (innerText) {
+		localStorage.setItem('rock-paper-scissors-frontendmentor-app-score', innerText)
+
+		handleTitle(innerText)
+	}
+})
+
+observer.observe(score, {
+	attributes: true,
+	childList: true,
+	characterData: true,
+})
+
+getScoreFromLocal()
+
+document.addEventListener('click', (e) => {
+	if (e.target === openModal) {
+		const modal = `
+      <div class="modal">
+        <div class="modal-content">
+          <section class="modal-first">
+            <h2 class="title">Rules</h2>
+            <button class="modal-close" onclick="closeModal()">
+              <img src="./images/icon-close.svg" alt="X" />
+            </button>
+          </section>
+          <section class="modal-last">
+            <img src="./images/rules.png" alt="Rules schema" draggable="false" />
+          </section>
+        </div>
+      </div>`
+
+		modalContainer.innerHTML = modal
+	}
+
+	const localPick = e.target.getAttribute('data-pick')
+
+	// If the element clicked is one pick
+	if (localPick) {
+		// Get opponent pick and game result
+		const { opponentPick, gameResult } = game(localPick)
+
+		if (gameResult === 'win') {
+			score.innerHTML = Number(score.innerHTML) + 1
+		}
+
+		if (gameResult === 'loss') {
+			score.innerHTML = score.innerHTML > 0 ? Number(score.innerHTML) - 1 : 0
+		}
+
+		hideHome()
+		view.innerHTML = resultView({
+			gameResult,
+			localPick,
+			opponentPick,
+		})
+	}
+})
+
 // Results layout view
-const resultView = ({ gameResult, localPick, opponentPick }) => `
-  <div class="results">
+function resultView({ gameResult, localPick, opponentPick }) {
+	return `<div class="results">
     <div class="results-item">
       <div class="results-item-element ${gameResult === 'win' ? 'winner' : ''}">
         <div class="elements-item ${localPick}">
@@ -18,9 +92,7 @@ const resultView = ({ gameResult, localPick, opponentPick }) => `
     </div>
 
     <div class="results-item">
-      <div class="results-item-element ${
-        gameResult === 'loss' ? 'winner' : ''
-      }">
+      <div class="results-item-element ${gameResult === 'loss' ? 'winner' : ''}">
         <div class="elements-item ${opponentPick}">
         <picture class="elements-item-pic">
           <img
@@ -39,190 +111,114 @@ const resultView = ({ gameResult, localPick, opponentPick }) => `
 
     <div class="results-item-result">
       <h2 class="results-item-result-name">${
-        gameResult === 'loss'
-          ? 'You loss'
-          : gameResult === 'win'
-          ? 'You win'
-          : 'Tie'
-      }</h2>
+				gameResult === 'loss' ? 'You loss' : gameResult === 'win' ? 'You win' : 'Tie'
+			}</h2>
       <button class="button" onclick="playAgain()">Play again</button>
     </div>
-  </div>
-  `
+  </div>`
+}
 
 // Get opponent pick from user pick
-const game = (localPick) => {
-  console.log(localPick)
-  if (!localPick) {
-    console.error('Please pick one item')
-    return
-  }
+function game(localPick) {
+	if (!localPick) {
+		console.error('Please pick one item')
+		return
+	}
 
-  const picks = ['paper', 'scissors', 'rock']
+	const picks = ['paper', 'scissors', 'rock']
 
-  let opponentPick = picks[Math.floor(Math.random() * picks.length)]
+	let opponentPick = picks[Math.floor(Math.random() * picks.length)]
 
-  let gameResult
+	let gameResult
 
-  if (opponentPick === localPick) {
-    gameResult = 'tie'
-    return { opponentPick, gameResult }
-  }
+	if (opponentPick === localPick) {
+		gameResult = 'tie'
+		return { opponentPick, gameResult }
+	}
 
-  if (localPick == 'paper') {
-    if (opponentPick === 'rock') {
-      gameResult = 'win'
-    }
+	if (localPick == 'paper') {
+		if (opponentPick === 'rock') {
+			gameResult = 'win'
+		}
 
-    if (opponentPick === 'scissors') {
-      gameResult = 'loss'
-    }
-  }
+		if (opponentPick === 'scissors') {
+			gameResult = 'loss'
+		}
+	}
 
-  if (localPick == 'scissors') {
-    if (opponentPick === 'paper') {
-      gameResult = 'win'
-    }
-    if (opponentPick === 'rock') {
-      gameResult = 'loss'
-    }
-  }
+	if (localPick == 'scissors') {
+		if (opponentPick === 'paper') {
+			gameResult = 'win'
+		}
+		if (opponentPick === 'rock') {
+			gameResult = 'loss'
+		}
+	}
 
-  if (localPick == 'rock') {
-    if (opponentPick === 'scissors') {
-      gameResult = 'win'
-    }
-    if (opponentPick === 'paper') {
-      gameResult = 'loss'
-    }
-  }
-  return { opponentPick, gameResult }
+	if (localPick == 'rock') {
+		if (opponentPick === 'scissors') {
+			gameResult = 'win'
+		}
+		if (opponentPick === 'paper') {
+			gameResult = 'loss'
+		}
+	}
+	return { opponentPick, gameResult }
 }
 
 // Reset the view to play again
-const playAgain = () => {
-  view.innerHTML = firstView
+function playAgain() {
+	showHome()
+}
+
+function showHome() {
+	elements.classList.remove('hidden')
+	view.innerHTML = ''
+}
+
+function hideHome() {
+	elements.classList.add('hidden')
 }
 
 // Get and set the score if it exists in localStorage
-const getScoreFromLocal = () => {
-  localScore = JSON.parse(
-    localStorage.getItem('rock-paper-scissors-frontendmentor-app-score')
-  )
-  if (localScore) {
-    score.innerText = localScore
-  }
+function getScoreFromLocal() {
+	localScore = JSON.parse(localStorage.getItem('rock-paper-scissors-frontendmentor-app-score'))
+	if (localScore) {
+		score.innerText = localScore
+	}
 }
 
 // Change the title based on the score
-const handleTitle = (scoreValue) => {
-  let timeout
-  const generateTimeout = (fn, time) => {
-    timeout = setTimeout(fn, time)
-  }
+function handleTitle(scoreValue) {
+	let timeout
+	const generateTimeout = (fn, time) => {
+		timeout = setTimeout(fn, time)
+	}
 
-  const removeTimeout = () => {
-    clearTimeout(timeout)
-  }
+	const removeTimeout = () => {
+		clearTimeout(timeout)
+	}
 
-  const titleScoreOnly = `Score: ${scoreValue}`
-  const titleScorePlusOriginal = `${titleScoreOnly} - ${documentTitle}`
+	const titleScoreOnly = `Score: ${scoreValue}`
+	const titleScorePlusOriginal = `${titleScoreOnly} - ${documentTitle}`
 
-  document.title = titleScoreOnly
+	document.title = titleScoreOnly
 
-  generateTimeout(() => {
-    document.title = titleScorePlusOriginal
-  }, 2000)
+	generateTimeout(() => {
+		document.title = titleScorePlusOriginal
+	}, 2000)
 
-  generateTimeout(() => {
-    document.title = titleScoreOnly
-  }, 4000)
+	generateTimeout(() => {
+		document.title = titleScoreOnly
+	}, 4000)
 
-  generateTimeout(() => {
-    document.title = titleScorePlusOriginal
-  }, 6000)
-  generateTimeout()
-  removeTimeout()
+	generateTimeout(() => {
+		document.title = titleScorePlusOriginal
+	}, 6000)
+	generateTimeout()
+	removeTimeout()
 }
 
-const closeModal = () => {
-  modalContainer.innerHTML = ''
+function closeModal() {
+	modalContainer.innerHTML = ''
 }
-
-const documentTitle = document.title
-const modalContainer = document.getElementById('modal-container')
-const view = document.getElementById('view')
-
-// Save the first part of the view (pick select section)
-const firstView = view.innerHTML
-
-// Save each pick element and score
-const paper = document.getElementById('paper')
-const scissors = document.getElementById('scissors')
-const rock = document.getElementById('rock')
-const score = document.getElementById('score')
-const openModal = document.getElementById('open-modal')
-
-// Detect score changes for saving it in localStorage
-const observer = new MutationObserver((mutations) => {
-  const innerText = mutations[0].target.innerText
-  if (innerText) {
-    localStorage.setItem(
-      'rock-paper-scissors-frontendmentor-app-score',
-      innerText
-    )
-
-    handleTitle(innerText)
-  }
-})
-
-observer.observe(score, {
-  attributes: true,
-  childList: true,
-  characterData: true,
-})
-
-getScoreFromLocal()
-
-document.addEventListener('click', (e) => {
-  if (e.target === openModal) {
-    const modal = `
-      <div class="modal">
-        <div class="modal-content">
-          <section class="modal-first">
-            <h2 class="title">Rules</h2>
-            <button class="modal-close" onclick="closeModal()">
-              <img src="./images/icon-close.svg" alt="X" />
-            </button>
-          </section>
-          <section class="modal-last">
-            <img src="./images/rules.png" alt="Rules schema" draggable="false" />
-          </section>
-        </div>
-      </div>`
-
-    modalContainer.innerHTML = modal
-  }
-
-  const localPick = e.target.getAttribute('data-pick')
-
-  // If the element clicked is one pick
-  if (localPick) {
-    // Get opponent pick and game result
-    const { opponentPick, gameResult } = game(localPick)
-
-    if (gameResult === 'win') {
-      score.innerHTML = Number(score.innerHTML) + 1
-    }
-
-    if (gameResult === 'loss') {
-      score.innerHTML = score.innerHTML > 0 ? Number(score.innerHTML) - 1 : 0
-    }
-
-    view.innerHTML = resultView({
-      gameResult,
-      localPick,
-      opponentPick,
-    })
-  }
-})
